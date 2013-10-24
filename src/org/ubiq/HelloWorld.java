1891340e06659004393a5d788a7baa7b910e11b8
Mufid Afif <mufid@progresioapp.com>
2013-10-24 07:37:45 +0700

    Initial commit

---
+++ src/org/ubiq/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.ubiq;
+import org.osgi.framework.*;
+
+public class HelloWorld {	 
+	public class HelloActivator implements BundleActivator {
+	  public void start(BundleContext context) {
+	    System.out.println("Hello EclipseZone Readers!");
+	  }
+	 
+	  public void stop(BundleContext context) {
+	    System.out.println("Goodbye EclipseZone Readers!");
+	  }
+	}
+}
